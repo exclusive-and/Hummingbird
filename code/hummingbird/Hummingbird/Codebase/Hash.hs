25286cc2e4c61ee8ce50171968fe7c59b60d338f
exclusive-and <exclusiveandgate@gmail.com>
2026-05-25 16:51:19 -0400

    Add Codebase.Hash ByteArrayAccess and Hashable instances

--- code/hummingbird/Hummingbird/Codebase/Hash.hs
+++ code/hummingbird/Hummingbird/Codebase/Hash.hs
@@ -1,21 +1,24 @@
 module Hummingbird.Codebase.Hash where
 
+import Data.Array.Byte
 import Data.Binary
 import Data.ByteArray qualified as ByteArray
 import Data.ByteString (ByteString)
-import Data.ByteString qualified as ByteString
+import Data.ByteString.Short (ShortByteString)
+import Data.Hashable
 import Data.Text qualified as Text
 import Crypto.Hash
 import Prelude
 import Prettyprinter
+import Unsafe.Coerce (unsafeCoerce)
 
 newtype Hash = Hash (Crypto.Hash.Digest SHA3_512)
   deriving stock
     ( Eq
-    , Generic
     , Ord
     , Show
     )
+  deriving newtype (ByteArray.ByteArrayAccess)
 
 instance Binary Hash where
   get = do
@@ -26,6 +29,15 @@ instance Binary Hash where
   
   put (Hash digest) =
     put @ByteString $ ByteArray.convert digest
+
+instance Hashable Hash where
+  salt `hashWithSalt` Hash digest =
+    hashWithSalt
+      salt
+      (unsafeCoerce @_ @ByteArray digest)
+    -- 'unsafeCoerce' is a necessary evil to make this function work. Digest and
+    -- ByteArray are representationally equal under the hood; crypton hides
+    -- the equality from us to protect the integrity of the individual hashes.
 
 instance Pretty Hash where
   pretty (Hash a) =
