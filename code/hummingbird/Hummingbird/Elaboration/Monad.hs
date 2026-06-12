fce35e970a22104e995d1edde2805c6969b54a10
exclusive-and <exclusiveandgate@gmail.com>
2026-06-12 12:01:46 -0400

    Update Fetch module imports

--- code/hummingbird/Hummingbird/Elaboration/Monad.hs
+++ code/hummingbird/Hummingbird/Elaboration/Monad.hs
@@ -3,8 +3,7 @@ module Hummingbird.Elaboration.Monad where
 import Control.Monad
 import Prelude
 
-import Fetch (Task)
-
+import Hummingbird.Fetch as Fetch
 import Hummingbird.Query (Query)
 
 newtype M a = M (Task Query IO a)
