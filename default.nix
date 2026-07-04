480b799e8265977bd40ef43566c0bd75923c2261
exclusive-and <60526169+exclusive-and@users.noreply.github.com>
2026-07-04 15:14:57 -0400

    Upgrade Nix build scripts (#8)
    - Updates the nixpkgs input pins.
    - Removes clutter in the root directory.
    - Replaces old scripts with (hopefully) more composable scripts based on the callPackage pattern.

--- default.nix
+++ default.nix
@@ -1,52 +1,9 @@
 {
-  sources ? import ./nix/sources.nix
+  ghcVersion ? "ghc912"
+, pkgs ? import ./nix/nixpkgs.nix {}
 , ...
 }@args:
 
-let
-
-  defaultArgs = {
-    lib = import "${sources.nixpkgs}/lib";
-    nixpkgs = import sources.nixpkgs {};
-  };
-
-  project = {
-    name = "hummingbird";
-    src = ./.;
-    version = "0";
-    depends = haskellPackages: with haskellPackages; [
-      ansi-terminal
-      base
-      binary
-      brick
-      bytestring
-      constraints-extras
-      containers
-      crypton_1_1_1
-      dependent-sum
-      dependent-sum-template
-      filepath
-      exceptions_0_10_12
-      hashable
-      haskeline
-      monad-chronicle
-      mtl
-      parsec
-      prettyprinter
-      prettyprinter-ansi-terminal
-      primitive
-      ram
-      shellmet
-      some
-      template-haskell
-      text
-      text-rope
-      these
-      transformers
-      unordered-containers
-      vty
-    ];
-  };
-
-in
-  import ./scripts/build.nix (defaultArgs // args) project
+pkgs.callPackage ./nix/package.nix {
+  haskellPackages = pkgs.haskell.packages."${ghcVersion}";
+}
