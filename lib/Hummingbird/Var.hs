a2fcf32543517a57263c128f4d0c908e02529704
exclusive-and <exclusiveandgate@gmail.com>
2026-07-23 14:52:59 -0400

    Rename VarInfo to VarDetails

--- lib/Hummingbird/Var.hs
+++ lib/Hummingbird/Var.hs
@@ -12,16 +12,18 @@ import Data.Set qualified as Set
 
 import Hummingbird.Builtin as Builtin
 import Hummingbird.Codebase.Hash
-import Hummingbird.Codebase.Id
+import Hummingbird.Codebase.Id as Codebase
 import Hummingbird.Name (Name)
 import Hummingbird.Name qualified as Name
 
-data VarInfo
+type Uniq = Int
+
+data VarDetails
   = NoInfo
   | Hole
   | KnownPrim !Builtin
-  | Local !Int
-  | Hashed !Id
+  | Local !Uniq
+  | Hashed !Codebase.Id
   deriving
     ( Eq
     , Generic
@@ -31,9 +33,9 @@ data VarInfo
 
 -- |
 data Var = Var
-  { name  :: !Name
-  , uniq  :: !Int
-  , info  :: !VarInfo
+  { name    :: !Name
+  , uniq    :: !Uniq
+  , details :: !VarDetails
   }
   deriving (Show)
 
@@ -49,23 +51,29 @@ instance Hashable Var where
 
 instance Pretty Var where
   pretty Var{..} =
-    case info of
+    case details of
       NoInfo          -> pretty name <> brackets (pretty uniq)
       Hole            -> "hole:" <> pretty uniq
       KnownPrim prim  -> "built-in:" <> pretty prim
       Local x         -> "stable:" <> pretty x
       Hashed hash     -> "hash:" <> pretty name <> brackets (pretty hash)
 
+isVarBuiltin :: Var -> Bool
+isVarBuiltin var =
+  case details var of
+    KnownPrim _ -> True
+    _           -> False
+
 type InScope = Set Var
 type FreeVars = Set Var
 
-mkVarNoInfo :: Int -> Name -> Var
+mkVarNoInfo :: Uniq -> Name -> Var
 mkVarNoInfo uniq name = Var name uniq NoInfo
 
-mkVarHashed :: Int -> Name -> Id -> Var
+mkVarHashed :: Uniq -> Name -> Id -> Var
 mkVarHashed uniq name hash = Var name uniq (Hashed hash)
 
-mkVarBuiltin :: Int -> Name -> Builtin -> Var
+mkVarBuiltin :: Uniq -> Name -> Builtin -> Var
 mkVarBuiltin uniq name prim = Var name uniq (KnownPrim prim)
 
 builtinVars :: [Var]
@@ -80,8 +88,3 @@ builtinVars =
   , mkVarBuiltin 8 (Name.Name "cake") Cake
   , mkVarBuiltin 0 (Name.Name "placeholder") Placeholder
   ]
-
-isBuiltin :: Var -> Bool
-isBuiltin Var{info} = case info of
-  KnownPrim _ -> True
-  _           -> False
